9181176a0d14219672c13ab3db471c2fb85d36ad
Kachorroski <joabejbk@gmail.com>
2016-11-06 12:33:31 -0200

    melhorando a tabela de listagem de produto.

--- produto-lista.php
+++ produto-lista.php
@@ -1,15 +1,20 @@
-<?php include("cabecalho.php");?>
-
-<?php include("conecta.php");
-
-include("banco-produto.php");
+<?php include("cabecalho.php");
+include("conecta.php");
+include("banco-produto.php"); ?>
 
 
-$produtos = listaProdutos($conexao);
-foreach ($produtos as $produto){
-    echo $produto['nome'] . "<br/>";
-}
+<table class="table table-striped table-bordered">
+    <?php
+    $produtos = listaProdutos($conexao);
+    foreach ($produtos as $produto) :
+        ?>
+        <tr>
+            <td><?= $produto['nome'] ?></td>
+            <td><?= $produto['preco'] ?></td>
+        </tr>
+        <?php
+    endforeach;
+    ?>
+</table>
 
-?>
-
-<?php include("rodape.php");?>
+<?php include("rodape.php"); ?>
