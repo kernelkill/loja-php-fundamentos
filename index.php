3549dcad33d88f6864e1d6217e960f7eb39c426f
Kachorroski <joabejbk@gmail.com>
2016-11-13 21:42:10 -0200

    Verificando se o login é false ou true para realizar o acesso do usuario.

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php include("cabecalho.php"); ?>
-<h1>Bem-Vindo!</h1>
+<?php if(isset($_GET["login"]) && $_GET["login"] == true) { ?>
+    <p class="alert-success"> Login efetuado com sucesso.</p>
+<?php } ?>
 
+<?php if(isset($_GET["login"]) && $_GET["login"] == false) { ?>
+    <p class="alert-success"> Usuario ou Senha Invalidos.</p>
+<?php } ?>
+
+<h1>Bem-Vindo!</h1>
 <h2>Login</h2>
 <form action="login.php" method="post">
     <table class="table">
-    <tr>
-        <td>Email</td>
-        <td><input class="form-control" type="email" name="email"></td>
-    </tr>
-    <tr>
-        <td>Senha</td>
-        <td><input class="form-control" type="password" name="senha"></td>
-    </tr>
-    <tr>
-        <td>
-            <button class="btn btn-primary">Logar</button>
-        </td>
-    </tr>
-</table>
+        <tr>
+            <td>Email</td>
+            <td><input class="form-control" type="email" name="email"></td>
+        </tr>
+        <tr>
+            <td>Senha</td>
+            <td><input class="form-control" type="password" name="senha"></td>
+        </tr>
+        <tr>
+            <td>
+                <button class="btn btn-primary">Logar</button>
+            </td>
+        </tr>
+    </table>
 </form>
 <?php include("rodape.php"); ?>
