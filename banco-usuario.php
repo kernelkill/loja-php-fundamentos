d874660e37d56a342f3f88a02c1bfe195de3eaa8
Kachorroski <joabejbk@gmail.com>
2016-11-13 21:35:05 -0200

    Criando funçoes e fazendo validaçoes na busca de usuarios.

--- banco-usuario.php
+++ banco-usuario.php
@@ -5,3 +5,12 @@
  * Date: 13/11/16
  * Time: 20:57
  */
+
+
+function buscaUsuario($conexao, $email, $senha){
+    $senhaMd5 = md5($senha);
+    $query = "select * from usuarios where email='{$email}' and senha='{$senhaMd5}'";
+    $resultado = mysqli_query($conexao, $query);
+    $usuario = mysqli_fetch_assoc($resultado);
+    return $usuario;
+}
