f7a71fafc5681d8e7e4e5703d77fc5745ef5c250
Kachorroski <joabejbk@gmail.com>
2016-11-13 20:53:39 -0200

    Criando a tela de login.

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php include("cabecalho.php");?>
-            <h1>Bem-Vindo!</h1>
-<?php include("rodape.php");?>
+<?php include("cabecalho.php"); ?>
+<h1>Bem-Vindo!</h1>
+
+<h2>Login</h2>
+<form action="login.php" method="post">
+    <table class="table">
+    <tr>
+        <td>Email</td>
+        <td><input class="form-control" type="email" name="email"></td>
+    </tr>
+    <tr>
+        <td>Senha</td>
+        <td><input class="form-control" type="password" name="senha"></td>
+    </tr>
+    <tr>
+        <td>
+            <button class="btn btn-primary">Logar</button>
+        </td>
+    </tr>
+</table>
+</form>
+<?php include("rodape.php"); ?>
