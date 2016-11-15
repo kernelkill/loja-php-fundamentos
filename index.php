9be1af7270a9a9f64fba5aeb5015601d01bfb590
Kachorroski <joabejbk@gmail.com>
2016-11-15 14:22:45 -0200

    colocando tempo na sessao do cookie.

--- index.php
+++ index.php
@@ -10,25 +10,25 @@
 <h1>Bem-Vindo!</h1>
 <?php if (isset($_COOKIE["usuario_logado"])) { ?>
     <p class="text-success">Voce esta logado como <?= $_COOKIE["usuario_logado"] ?>.</p>
-<?php }else{ ?>
+<?php } else { ?>
 
-<h2>Login</h2>
-<form action="login.php" method="post">
-    <table class="table">
-        <tr>
-            <td>Email</td>
-            <td><input class="form-control" type="email" name="email"></td>
-        </tr>
-        <tr>
-            <td>Senha</td>
-            <td><input class="form-control" type="password" name="senha"></td>
-        </tr>
-        <tr>
-            <td>
-                <button class="btn btn-primary">Logar</button>
-            </td>
-        </tr>
-    </table>
-</form>
-<?php }?>
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <table class="table">
+            <tr>
+                <td>Email</td>
+                <td><input class="form-control" type="email" name="email"></td>
+            </tr>
+            <tr>
+                <td>Senha</td>
+                <td><input class="form-control" type="password" name="senha"></td>
+            </tr>
+            <tr>
+                <td>
+                    <button class="btn btn-primary">Logar</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+<?php } ?>
 <?php include("rodape.php"); ?>
