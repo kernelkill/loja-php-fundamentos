6a879f8725249890bc0019ad94392cfb0030c39a
Kachorroski <kernelkill@outlook.com>
2016-11-16 20:34:45 -0200

    Validando a segurança nas requisiçoes.

--- produto-lista.php
+++ produto-lista.php
@@ -1,17 +1,14 @@
 <?php include("cabecalho.php");
 include("conecta.php");
 include("banco-produto.php");
-include ("valida-usuario.php");
-
+include("valida-usuario.php");
 verificaUsuarioLogado();
-
 ?>
 
-<?php if (array_key_exists("removido", $_GET) && $_GET['removido'] == 'true') {
-    ?>
-    <p class="alert-success">Produto apagado com sucesso.</p>
-    <?php
-}
+<?php if (isset($_SESSION["success"])) { ?>
+    <p class="alert-success"> <?= $_SESSION["success"]?></p>
+<?php }
+unset($_SESSION["success"]);
 ?>
 
 <table class="table table-striped table-bordered">
