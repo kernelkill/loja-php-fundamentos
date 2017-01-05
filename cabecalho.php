cb9073d3c4ba737127ca6ec2a9cd02d580267079
Kachorroski <kernelkill@outlook.com>
2017-01-04 22:07:17 -0300

    Arrumando O CSS

--- cabecalho.php
+++ cabecalho.php
@@ -9,7 +9,7 @@ require_once("mostra-alerta.php"); ?>
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="css/bootstrap.css">
-        <!--<link rel="stylesheet" href="css/loja.css">-->
+        <link rel="stylesheet" href="css/loja.css">
         <link rel="stylesheet" href="css/form-login.css">
         <title>Loja</title>
     </head>
--- produto-formulario.php
+++ produto-formulario.php
@@ -9,7 +9,7 @@ $usado = "";
 $categorias = listaCategorias($conexao);
 ?>
 
-    <h1>Formulario de Produto</h1>
+    <h1 id="tituloinsereproduto">Formulario de Produto</h1>
     <form action="adiciona-produto.php" method="post">
         <table class="table">
 
